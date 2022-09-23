19597dd41d55639da7e44ae94cd2dcb680693896
DeepakR-Sastry <mamillap@ualberta.ca>
2022-09-23 12:45:03 -0600

    edited files

--- echo_server.py
+++ echo_server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import socket
 import time
+from multiprocessing import Process
 
 #define address & buffer size
-HOST = ""
+HOST = "www.google.com"
 PORT = 8001
 BUFFER_SIZE = 1024
 
@@ -22,12 +23,15 @@ def main():
         while True:
             conn, addr = s.accept()
             print("Connected by", addr)
+            p = Process(target=handle_echo)
             
-            #recieve data, wait a bit, then send it back
-            full_data = conn.recv(BUFFER_SIZE)
-            time.sleep(0.5)
-            conn.sendall(full_data)
-            conn.close()
+
+def handle_echo():
+    #recieve data, wait a bit, then send it back
+    full_data = conn.recv(BUFFER_SIZE)
+    time.sleep(0.5)
+    conn.sendall(full_data)
+    conn.close()
 
 if __name__ == "__main__":
     main()
